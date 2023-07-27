958940a0a8f81ffd29d14236a5c9574fd264a99d
faisalrehman <faisalfasi18@gmail.com>
2023-07-27 18:22:36 +0200

    add json file

---
+++ src/pages/projectDetails.js
@@ -0,0 +1,36 @@
+const projectDetails = [
+  {
+    title: "Portfolio Site",
+    madeWith: ["HTML5", "BootStrap v5"],
+    description:
+      "Welcome to my portfolio website, a showcase of my diverse skills and passion for web development where you can discover my projects and expertise in front-end and UI/UX..",
+    projectImg: "/images/portfolio-img.png",
+    paddingLeftRight: "lg:pl-[15%] lg:pr-24",
+    titlePos: "lg:left-0",
+    websiteLink: "https://faisalrehman-portfolio.netlify.app",
+    gitHubLink: "https://github.com/FaisalFasi/portfolio-site",
+  },
+  {
+    title: "Netflix Clone",
+    madeWith: ["React", "TMDB Api", "FireBase"],
+    description:
+      "Discover your favorite movies and shows on my Netflix clone,providing a responsive and visually appealing platform for endless entertainment at your fingertips",
+    projectImg: "/images/netflix-img.png",
+    paddingLeftRight: "lg:pr-[15%] lg:pl-24",
+    titlePos: "lg:right-0",
+    websiteLink: "https://netflix-clone-version-0.netlify.app",
+    gitHubLink: "https://github.com/FaisalFasi/react-netflix-clone-project",
+  },
+  {
+    title: "TODO APP",
+    madeWith: ["React", "Tailwind"],
+    description:
+      "A simple and intuitive todo app to help you organize tasks and stay productive, with the ability to add, delete, and mark tasks as complete",
+    projectImg: "/images/todoApp-img.png",
+    paddingLeftRight: "lg:pl-[15%] lg:pr-24",
+    titlePos: "lg:left-0",
+    websiteLink: "https://faisalrehman-todoapp.netlify.app/",
+    gitHubLink: "https://github.com/FaisalFasi/react-todo-list-project",
+  },
+];
+export default projectDetails;

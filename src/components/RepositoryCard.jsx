d76721ddf46779a023a37489a392724c0ec8093c
faisalrehman <faisalfasi18@gmail.com>
2023-07-27 18:04:07 +0200

    projects links updated with descriptions

--- src/components/RepositoryCard.jsx
+++ src/components/RepositoryCard.jsx
@@ -5,7 +5,7 @@ const RepositoryCard = (props) => {
     <a
       href={props.repo.projectLink}
       target="_blank"
-      className="flex flex-col items-start w-full lg:w-[50%] mt-8 px-6 py-4 shadow-lg drop-shadow-lg border border-solid rounded-[8px] hover:-translate-y-1  transition-all duration-200"
+      className="flex flex-col items-start w-full   mt-8 px-6 py-4 shadow-lg drop-shadow-lg border border-solid rounded-[8px] hover:-translate-y-1  transition-all duration-200"
     >
       <div className="flex items-center justify-center gap-4">
         <h1 className="font-bold text-lg">{props.repo.title} </h1>
@@ -19,6 +19,3 @@ const RepositoryCard = (props) => {
 };
 
 export default RepositoryCard;
-{
-  /* <a href={props.repo.projectLink} target="_blank"> */
-}
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -5,60 +5,8 @@ import Card from "../components/Card";
 import RepositoryCard from "../components/RepositoryCard";
 import Footer from "../components/Footer";
 import { ThemeContext } from "../context/ThemeContext.jsx";
-
-{
-  /* <Link to={"/"}>Projects</Link> */
-}
-const projectDetails = [
-  {
-    title: "Portfolio Site",
-    madeWith: ["HTML5", "BootStrap v5"],
-    description:
-      "Welcome to my portfolio website, a showcase of my diverse skills and passion for web development where you can discover my projects and expertise in front-end and UI/UX..",
-    projectImg: "/images/portfolio-img.png",
-    paddingLeftRight: "lg:pl-[15%] lg:pr-24",
-    titlePos: "lg:left-0",
-    websiteLink: "https://faisalrehman-portfolio.netlify.app",
-    gitHubLink: "https://github.com/FaisalFasi/portfolio-site",
-  },
-  {
-    title: "Netflix Clone",
-    madeWith: ["React", "TMDB Api", "FireBase"],
-    description:
-      "Discover your favorite movies and shows on my Netflix clone,providing a responsive and visually appealing platform for endless entertainment at your fingertips",
-    projectImg: "/images/netflix-img.png",
-    paddingLeftRight: "lg:pr-[15%] lg:pl-24",
-    titlePos: "lg:right-0",
-    websiteLink: "https://netflix-clone-version-0.netlify.app",
-    gitHubLink: "https://github.com/FaisalFasi/react-netflix-clone-project",
-  },
-  {
-    title: "TODO APP",
-    madeWith: ["React", "Tailwind"],
-    description:
-      "A simple and intuitive todo app to help you organize tasks and stay productive, with the ability to add, delete, and mark tasks as complete",
-    projectImg: "/images/todoApp-img.png",
-    paddingLeftRight: "lg:pl-[15%] lg:pr-24",
-    titlePos: "lg:left-0",
-    websiteLink: "https://faisalrehman-todoapp.netlify.app/",
-    gitHubLink: "https://github.com/FaisalFasi/react-todo-list-project",
-  },
-];
-
-const repositoriesInfo = [
-  {
-    title: "Portfolio",
-    language: "ReactJS",
-    description: "Personal website build with ReactJS and Tailwind CSS.",
-    projectLink: "https://github.com/FaisalFasi/Faisal-Rehman-Portfolio",
-  },
-  {
-    title: "Piano App",
-    language: "ReactJS",
-    description: "Piano app build with ReactJS and Tailwind CSS.",
-    projectLink: "https://github.com/FaisalFasi/react-piano-project",
-  },
-];
+import projectDetails from "./projectDetails.json";
+import repositoriesInfo from "./repositoriesInfo.json";
 
 const Projects = () => {
   const { isDarkMode } = useContext(ThemeContext);
@@ -131,7 +79,8 @@ const Projects = () => {
           </a>
         </div>
       </div>
-      <div className="flex flex-col md:flex-row gap-0 md:gap-12 mx-6 md:mx-8 lg:mx-20 mt-8 mb-20">
+      {/* <div className="flex flex-col md:flex-row gap-0 md:gap-12 mx-6 md:mx-8 lg:mx-20 mt-8 mb-20"> */}
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-12 mx-6 md:mx-8  mt-8 mb-20">
         {repositoriesInfo.map((repo, idx) => {
           return <RepositoryCard key={idx} repo={repo} />;
         })}

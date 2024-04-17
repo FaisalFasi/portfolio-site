48242f74638d5e155084907cfdcd1d7498f5fb52
faisalrehman <faisalfasi18@gmail.com>
2024-04-17 16:09:40 +0200

    remove a project link from projects which is not working due to free server limit is over

--- src/utils/projectDetails.js
+++ src/utils/projectDetails.js
@@ -44,27 +44,28 @@ const projectDetails = [
     gitHubLink: "https://github.com/FaisalFasi/mongodb-snippet-client",
   },
   {
-    title: "FR-Shop",
-    madeWith: ["React", "Tailwind", "Strapi", "Stripe", "PostgreSQL"],
-    description:
-      "Discover FR-Shop, where online shopping meets seamless user experience. This project is user-friendly e-commerce platforms. With features like product management, and secure checkout, the site provides a streamlined shopping journey. This websites server is down so please check the github repo",
-
-    projectImg: "/images/fr-Shop.png",
-    paddingLeftRight: "lg:pl-[15%] lg:pr-24",
-    titlePos: "lg:right-0",
-    websiteLink: "https://fr-shop.netlify.app/",
-    gitHubLink: "https://github.com/FaisalFasi/FR-Shop",
-  },
-  {
     title: "TODO APP",
     madeWith: ["React", "Tailwind", "Context"],
     description:
       "A simple and intuitive todo app to help you organize tasks and stay productive, with the ability to add, delete, and mark tasks as complete",
     projectImg: "/images/todoApp-img.png",
-    paddingLeftRight: "lg:pr-[15%] lg:pl-24",
-    titlePos: "lg:left-0",
+    paddingLeftRight: "lg:pl-[15%] lg:pr-24",
+    titlePos: "lg:right-0",
     websiteLink: "https://faisalrehman-todoapp.netlify.app/",
     gitHubLink: "https://github.com/FaisalFasi/react-todo-list-project",
   },
 ];
 export default projectDetails;
+
+// {
+//   title: "FR-Shop",
+//   madeWith: ["React", "Tailwind", "Strapi", "Stripe", "PostgreSQL"],
+//   description:
+//     "Discover FR-Shop, where online shopping meets seamless user experience. This project is user-friendly e-commerce platforms. With features like product management, and secure checkout, the site provides a streamlined shopping journey. This websites server is down so please check the github repo",
+
+//   projectImg: "/images/fr-Shop.png",
+//   paddingLeftRight: "lg:pl-[15%] lg:pr-24",
+//   titlePos: "lg:right-0",
+//   websiteLink: "https://fr-shop.netlify.app/",
+//   gitHubLink: "https://github.com/FaisalFasi/FR-Shop",
+// },

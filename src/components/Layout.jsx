2f85a6772780cd816fbaeb708004e312e0168552
faisalrehman <faisalfasi18@gmail.com>
2024-09-10 11:44:19 +0200

    chore: Remove unused dependencies and components

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,8 +1,6 @@
 import { Outlet } from "react-router-dom";
-// import Chatbot from "./components/Chatbot"; // Add your chatbot component
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-// import ChatBotPress from "../components/ChatBotPress";
 const Layout = () => {
   return (
     <div>
@@ -10,7 +8,6 @@ const Layout = () => {
       <main>
         <Outlet /> {/* This will render the current page content */}
       </main>
-      {/* <ChatBotPress /> */}
       {/* Chatbot is added here and will be visible on every page */}
       <Footer />
     </div>

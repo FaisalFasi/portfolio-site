4e3c94ac3c2228745d426927d1b58c416d8aaa7c
faisalrehman <faisalfasi18@gmail.com>
2024-10-06 15:11:42 +0200

    feat: add framer-motion and typewriter-effect for enhanced animations and text effects on Home page

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,137 +1,248 @@
 import React, { useEffect, useState, useContext } from "react";
-import { Typewriter } from "react-simple-typewriter";
+import Typewriter from "typewriter-effect";
 import { ThemeContext } from "../context/ThemeContext";
+import { motion } from "framer-motion";
 
 const AboutMe = () => {
   const { isDarkMode } = useContext(ThemeContext);
   const [typescriptCompleted, setTypescriptCompleted] = useState(false);
-  const [count, setCount] = useState(0);
-  const [mobileScreen, setMobileScreen] = useState(false);
+  const [visibleLineIndex, setVisibleLineIndex] = useState(0); // State to track which line is visible
 
-  const handleCount = () => {
-    setCount((prev) => prev + 1);
-  };
+  // Combine headings and paragraphs into an array of objects for easier mapping
+  const aboutContent = [
+    { title: "learnAboutMe()", content: "Loading data..." },
+    { title: "currentLocation", content: '"Berlin, Germany"' },
+    {
+      title: "interests",
+      content: '["Web Development", "Travelling", "Gaming", "Photography"]',
+    },
+    {
+      title: "education",
+      content:
+        '"Bachelor of Science in Computer Science - Hajvery University Lahore"',
+    },
+    {
+      title: "skills",
+      content:
+        '["JavaScript","ReactJs", "NextJs", "NodeJs", "Express", "Tailwind"]',
+    },
+    {
+      title: "contactMe",
+      content: '["Github", "Facebook", "LinkedIn", "Instagram"]',
+    },
+  ];
+
   useEffect(() => {
-    if (count >= 27) {
-      setTypescriptCompleted(true);
+    if (typescriptCompleted && visibleLineIndex < aboutContent.length) {
+      const timer = setTimeout(() => {
+        setVisibleLineIndex((prevIndex) => prevIndex + 1);
+      }, 100); // Delay before showing the next line
+
+      return () => clearTimeout(timer);
     }
-  });
-
-  // check when window size is greater then medium
-  const checkWindowSize = () => {
-    if (window.innerWidth < 640) {
-      setMobileScreen(true);
-    } else {
-      setMobileScreen(false);
-    }
-  };
-
-  useEffect(() => {
-    checkWindowSize();
-
-    // Add event listener for "resize" event on the window object
-    window.addEventListener("resize", checkWindowSize);
-
-    // Clean up the event listener on component unmount
-    return () => {
-      window.removeEventListener("resize", checkWindowSize);
-    };
-  }, [window.innerWidth]);
+  }, [typescriptCompleted, visibleLineIndex]);
 
   return (
-    <div>
-      <div
-        className={`relative flex justify-center  items-center h-10 mt-4 bg-[#D9D9D9] rounded-t-[20px] ${
+    <motion.div>
+      <motion.div
+        className={`relative flex justify-center items-center h-10 mt-4 bg-[#D9D9D9] rounded-t-[20px] ${
           isDarkMode
-            ? " bg-dark-bgHeaderAboutMe text-dark-text"
-            : "  text-light-text"
-        }  `}
+            ? "bg-dark-bgHeaderAboutMe text-dark-text"
+            : "text-light-text"
+        }`}
       >
-        {!mobileScreen ? (
-          <div className="w-full absolute flex justify-start items-center z-1 px-6  gap-2">
-            <div className="w-3 h-3 bg-red-500 rounded-full "></div>
-            <div className="w-3 h-3 bg-yellow-500 rounded-full"></div>
-            <div className="w-3 h-3 bg-green-600 rounded-full"></div>
-          </div>
-        ) : (
-          ""
-        )}
+        <div className="w-full absolute flex justify-start items-center z-1 px-6 gap-2">
+          <div className="w-3 h-3 bg-red-500 rounded-full"></div>
+          <div className="w-3 h-3 bg-yellow-500 rounded-full"></div>
+          <div className="w-3 h-3 bg-green-600 rounded-full"></div>
+        </div>
 
-        <p className="pl-4 sm:pl-0 z-10 font-bold ">
-          <Typewriter
-            words={[" Executed: introduceSelf.js"]}
-            loop={1}
-            cursor
-            cursorStyle="_"
-            typeSpeed={30}
-            deleteSpeed={10}
-            delaySpeed={1000}
-            // onLoopDone={handleDone}
-            onType={handleCount}
-          />
-        </p>
-      </div>
+        <motion.div className="pl-4 sm:pl-0 z-10 font-bold ">
+          <motion.h1
+            className={`py-2 font-poppinFont font-bold text-sm md:text-lg ${
+              isDarkMode ? "bg-dark-bgBodyAboutMe" : "text-light-text"
+            }`}
+            initial={{ y: -20, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+          >
+            <Typewriter
+              onInit={(typewriter) => {
+                typewriter
+                  .typeString("Executed: introduceSelf.js")
+                  .callFunction(() => {
+                    setTypescriptCompleted(true);
+                  })
+                  .start();
+              }}
+              options={{
+                delay: 20, // Typing speed (in milliseconds)
+              }}
+            />
+          </motion.h1>
+        </motion.div>
+      </motion.div>
+
       <div
-        className={`bg-[#EBEBEB]  rounded-b-[20px] p-6 font-nuntinoFont ${
+        className={`bg-[#EBEBEB] rounded-b-[20px] p-6 font-nuntinoFont ${
           typescriptCompleted ? "" : "h-[450px]"
-        }  ${isDarkMode ? " bg-dark-bgBodyAboutMe" : "  text-light-text"}`}
+        } ${isDarkMode ? "bg-dark-bgBodyAboutMe" : "text-light-text"}`}
       >
-        {typescriptCompleted ? (
+        {typescriptCompleted && (
           <div>
-            <h1>
-              <b className="text-[#B693F4]"> {">"}</b> &nbsp; learnAboutMe()
-            </h1>
-            <p className="py-2  font-poppinFont text-[#718096]">
-              Loading data...
-            </p>
-            <h1>
-              <b className="text-[#B693F4]"> {">"}</b> &nbsp; currentLocation
-            </h1>
-            <p className=" py-2  font-nuntinoFont text-[#718096]">
-              "Berlin, Germany"
-            </p>
-
-            <h1>
-              <b className="text-[#B693F4]"> {">"}</b> &nbsp; interests
-            </h1>
-            <p className="  py-2 font-nuntinoFont text-[#718096]">
-              ["Web Development", "Travelling", "Gaming", "Photography"]
-            </p>
-
-            <h1>
-              <b className="text-[#B693F4]"> {">"}</b> &nbsp; education
-            </h1>
-            <p className="  py-2 font-nuntinoFont text-[#718096]">
-              "Bachelor of Science in Computer Science - Hajvery University
-              Lahore"
-            </p>
-
-            <h1>
-              <b className="text-[#B693F4]"> {">"}</b> &nbsp; skills
-            </h1>
-            <p className="  py-2 font-nuntinoFont text-[#718096]">
-              ["JavaScript","ReactJs", "NextJs", "NodeJs", "Express",
-              "Tailwind"]
-            </p>
-
-            <h1>
-              <b className="text-[#B693F4]"> {">"}</b> &nbsp; contactMe
-            </h1>
-            <p className="  py-2 font-nuntinoFont text-[#718096]">
-              [
-              <span className="text-[#2aae66]">
-                &nbsp; "Github", &nbsp; "FaceBook", &nbsp; "LinkedIn", &nbsp;
-                "Instagram" &nbsp;
-              </span>
-              ]
-            </p>
+            {aboutContent.map((info, index) => (
+              <div key={index}>
+                <motion.h1
+                  initial={{ opacity: 0 }} // Start hidden
+                  animate={
+                    index <= visibleLineIndex ? { opacity: 1 } : { opacity: 0 }
+                  } // Fade in
+                  transition={{ duration: 0.02 }} // Duration of the animation
+                >
+                  <b className="text-[#B693F4]"> {">"}</b> &nbsp; {info.title}
+                </motion.h1>
+                <motion.p
+                  initial={{ opacity: 0 }} // Start hidden
+                  animate={
+                    index <= visibleLineIndex ? { opacity: 1 } : { opacity: 0 }
+                  } // Fade in
+                  transition={{ duration: 0.2 }} // Duration of the animation
+                  className="py-2 font-poppinFont text-[#718096]"
+                >
+                  {info.content}
+                </motion.p>
+              </div>
+            ))}
           </div>
-        ) : (
-          ""
         )}
       </div>
-    </div>
+    </motion.div>
   );
 };
 
 export default AboutMe;
+
+// import React, { useEffect, useState, useContext } from "react";
+// import Typewriter from "typewriter-effect";
+// import { ThemeContext } from "../context/ThemeContext";
+// import { motion } from "framer-motion";
+
+// const AboutMe = () => {
+//   const { isDarkMode } = useContext(ThemeContext);
+//   const [typescriptCompleted, setTypescriptCompleted] = useState(false);
+//   const [mobileScreen, setMobileScreen] = useState(false);
+
+//   const checkWindowSize = () => {
+//     setMobileScreen(window.innerWidth < 640);
+//   };
+
+//   useEffect(() => {
+//     checkWindowSize();
+//     window.addEventListener("resize", checkWindowSize);
+//     return () => {
+//       window.removeEventListener("resize", checkWindowSize);
+//     };
+//   }, []);
+
+//   return (
+//     <motion.div>
+//       <motion.div
+//         className={`relative flex justify-center items-center h-10 mt-4 bg-[#D9D9D9] rounded-t-[20px] ${
+//           isDarkMode
+//             ? "bg-dark-bgHeaderAboutMe text-dark-text"
+//             : "text-light-text"
+//         }`}
+//       >
+//         {!mobileScreen && (
+//           <div className="w-full absolute flex justify-start items-center z-1 px-6 gap-2">
+//             <div className="w-3 h-3 bg-red-500 rounded-full"></div>
+//             <div className="w-3 h-3 bg-yellow-500 rounded-full"></div>
+//             <div className="w-3 h-3 bg-green-600 rounded-full"></div>
+//           </div>
+//         )}
+//         <motion.div className="pl-4 sm:pl-0 z-10 font-bold ">
+//           <motion.h1
+//             className={`py-2 font-poppinFont font-bold text-sm md:text-lg  ${
+//               isDarkMode ? " bg-dark-bgBodyAboutMe" : "  text-light-text"
+//             }`}
+//             initial={{ y: -20, opacity: 0 }}
+//             animate={{ y: 0, opacity: 1 }}
+//             // transition={{ duration: 0.01 }}
+//           >
+//             <Typewriter
+//               onInit={(typewriter) => {
+//                 typewriter
+//                   .typeString("Executed: introduceSelf.js")
+//                   .callFunction(() => {
+//                     setTypescriptCompleted(true);
+//                   })
+//                   .start();
+//               }}
+//               options={{
+//                 delay: 30, // Faster typing speed (in milliseconds)
+//               }}
+//             />
+//           </motion.h1>
+//         </motion.div>
+//       </motion.div>
+//       <motion.div
+//         className={`bg-[#EBEBEB] rounded-b-[20px] p-6 font-nuntinoFont ${
+//           typescriptCompleted ? "" : "h-[450px]"
+//         } ${isDarkMode ? "bg-dark-bgBodyAboutMe" : "text-light-text"}`}
+//       >
+//         {typescriptCompleted && (
+//           <div>
+//             <h1>
+//               <b className="text-[#B693F4]"> {">"}</b> &nbsp; learnAboutMe()
+//             </h1>
+//             <p className="py-2 font-poppinFont text-[#718096]">
+//               Loading data...
+//             </p>
+//             <h1>
+//               <b className="text-[#B693F4]"> {">"}</b> &nbsp; currentLocation
+//             </h1>
+//             <p className="py-2 font-nuntinoFont text-[#718096]">
+//               "Berlin, Germany"
+//             </p>
+
+//             <h1>
+//               <b className="text-[#B693F4]"> {">"}</b> &nbsp; interests
+//             </h1>
+//             <p className="py-2 font-nuntinoFont text-[#718096]">
+//               ["Web Development", "Travelling", "Gaming", "Photography"]
+//             </p>
+
+//             <h1>
+//               <b className="text-[#B693F4]"> {">"}</b> &nbsp; education
+//             </h1>
+//             <p className="py-2 font-nuntinoFont text-[#718096]">
+//               "Bachelor of Science in Computer Science - Hajvery University
+//               Lahore"
+//             </p>
+
+//             <h1>
+//               <b className="text-[#B693F4]"> {">"}</b> &nbsp; skills
+//             </h1>
+//             <p className="py-2 font-nuntinoFont text-[#718096]">
+//               ["JavaScript","ReactJs", "NextJs", "NodeJs", "Express",
+//               "Tailwind"]
+//             </p>
+
+//             <h1>
+//               <b className="text-[#B693F4]"> {">"}</b> &nbsp; contactMe
+//             </h1>
+//             <p className="py-2 font-nuntinoFont text-[#718096]">
+//               [
+//               <span className="text-[#2aae66]">
+//                 &nbsp; "Github", &nbsp; "FaceBook", &nbsp; "LinkedIn", &nbsp;
+//                 "Instagram" &nbsp;
+//               </span>
+//               ]
+//             </p>
+//           </div>
+//         )}
+//       </motion.div>
+//     </motion.div>
+//   );
+// };
+
+// export default AboutMe;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import React, { useContext } from "react";
+import { motion } from "framer-motion";
 import AboutMe from "../components/AboutMe";
 import { ThemeContext } from "../context/ThemeContext";
+import Typewriter from "typewriter-effect";
 
 const technologies = [
   {
@@ -14,6 +16,7 @@ const technologies = [
   { name: "MongoDB", url: "https://www.mongodb.com/" },
   { name: "Tailwind CSS", url: "https://tailwindcss.com/" },
 ];
+
 const Home = () => {
   const { isDarkMode } = useContext(ThemeContext);
 
@@ -21,48 +24,82 @@ const Home = () => {
     <div
       className={`${
         isDarkMode
-          ? "bg-dark-background  text-dark-text"
-          : "bg-light-background  text-light-text"
+          ? "bg-dark-background text-dark-text"
+          : "bg-light-background text-light-text"
       }`}
     >
-      {/* <Header /> */}
-      <div id="home" className=" py-10 px-4 md:px-8 lg:px-[5%] xl:px-[15%]">
+      <div id="home" className="py-10 px-4 md:px-8 lg:px-[5%] xl:px-[15%]">
         <div className="flex flex-col lg:flex-row justify-center items-center">
-          <img
+          <motion.img
             src="/images/my-pic-6.png"
-            alt=""
+            alt="Faisal Rehman"
             className="w-60 h-60 rounded-[20px] border border-blue-100"
+            initial={{ opacity: 0, scale: 0.8 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.5 }}
           />
-          <div className="w-full py-12 px-2 lg:px-12 text-center  lg:text-left">
-            <h1 className=" py-2  font-poppinFont font-bold text-4xl text-[#5fc094]">
-              Hi, I'm Faisal Rehman!
-            </h1>
-            <p className="text-lg font-poppinFont flex flex-wrap justify-center lg:justify-start">
-              I am a Fullstack Developer having 3+ years of experience in
+          <div className="w-full py-12 px-2 lg:px-12 text-center lg:text-left">
+            <motion.h1
+              className="py-2 font-poppinFont font-bold text-4xl text-[#5fc094]"
+              initial={{ y: -20, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{ duration: 0.5 }}
+            >
+              <Typewriter
+                onInit={(typewriter) => {
+                  typewriter
+                    .typeString("Hi, I'm Faisal Rehman!")
+                    .pauseFor(2000) // Pause for 2 seconds
+                    .deleteAll() // Delete the current string
+                    .typeString("I'm a Fullstack Developer!") // Type a new string
+                    .pauseFor(2000) // Pause for 2 seconds
+                    .deleteAll()
+                    .typeString("I love creating interactive web applications.")
+                    .pauseFor(2000) // Pause for 2 seconds
+                    .start();
+                }}
+                options={{
+                  delay: 75, // Typing speed
+                }}
+              />
+            </motion.h1>
+            <motion.p
+              className="text-lg font-poppinFont flex flex-wrap justify-center lg:justify-start"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.5, delay: 0.2 }}
+            >
+              I am a Fullstack Developer with 3+ years of experience in
               developing state-of-the-art applications, building great user
-              experiences by bringing simplicity to life and constantly
+              experiences by bringing simplicity to life, and constantly
               learning. Skilled in technologies like
               <span className="flex flex-wrap justify-center lg:justify-start items-center">
                 {technologies.map((tech, index) => (
-                  <React.Fragment key={index}>
+                  <motion.span
+                    key={index}
+                    className={`px-2 rounded text-sm`}
+                    initial={{ opacity: 0, y: 10 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 0.5, delay: index * 0.1 }}
+                  >
                     <a
                       href={tech.url}
-                      className="font-bold text-[#2aae66] transition duration-300 ease-in-out transform hover:text-[#5fc094] hover:scale-110"
+                      className="font-bold text-base text-[#2aae66] transition duration-300 ease-in-out transform hover:text-[#78857f] hover:scale-110"
                       target="_blank"
                       rel="noopener noreferrer"
                     >
-                      &nbsp; {tech.name}
+                      &nbsp;{tech.name}
                     </a>
                     {index !== technologies.length - 1 ? "," : "."}
-                  </React.Fragment>
+                  </motion.span>
                 ))}
               </span>
               I am a team player and always looking for new challenges and ways
               to improve myself.
-            </p>
+            </motion.p>
           </div>
         </div>
-        {/* about me section  */}
+        {/* About Me Section */}
         <AboutMe />
       </div>
       {/* <Footer /> */}
@@ -71,3 +108,77 @@ const Home = () => {
 };
 
 export default Home;
+
+// import React, { useContext } from "react";
+// import AboutMe from "../components/AboutMe";
+// import { ThemeContext } from "../context/ThemeContext";
+
+// const technologies = [
+//   {
+//     name: "JavaScript",
+//     url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+//   },
+//   { name: "React.js", url: "https://react.dev/" },
+//   { name: "Next.js", url: "https://nextjs.org/" },
+//   { name: "Node.js", url: "https://nodejs.org/" },
+//   { name: "MySQL", url: "https://www.mysql.com/" },
+//   { name: "MongoDB", url: "https://www.mongodb.com/" },
+//   { name: "Tailwind CSS", url: "https://tailwindcss.com/" },
+// ];
+// const Home = () => {
+//   const { isDarkMode } = useContext(ThemeContext);
+
+//   return (
+//     <div
+//       className={`${
+//         isDarkMode
+//           ? "bg-dark-background  text-dark-text"
+//           : "bg-light-background  text-light-text"
+//       }`}
+//     >
+//       {/* <Header /> */}
+//       <div id="home" className=" py-10 px-4 md:px-8 lg:px-[5%] xl:px-[15%]">
+//         <div className="flex flex-col lg:flex-row justify-center items-center">
+//           <img
+//             src="/images/my-pic-6.png"
+//             alt=""
+//             className="w-60 h-60 rounded-[20px] border border-blue-100"
+//           />
+//           <div className="w-full py-12 px-2 lg:px-12 text-center  lg:text-left">
+//             <h1 className=" py-2  font-poppinFont font-bold text-4xl text-[#5fc094]">
+//               Hi, I'm Faisal Rehman!
+//             </h1>
+//             <p className="text-lg font-poppinFont flex flex-wrap justify-center lg:justify-start">
+//               I am a Fullstack Developer having 3+ years of experience in
+//               developing state-of-the-art applications, building great user
+//               experiences by bringing simplicity to life and constantly
+//               learning. Skilled in technologies like
+//               <span className="flex flex-wrap justify-center lg:justify-start items-center">
+//                 {technologies.map((tech, index) => (
+//                   <React.Fragment key={index}>
+//                     <a
+//                       href={tech.url}
+//                       className="font-bold text-[#2aae66] transition duration-300 ease-in-out transform hover:text-[#5fc094] hover:scale-110"
+//                       target="_blank"
+//                       rel="noopener noreferrer"
+//                     >
+//                       &nbsp; {tech.name}
+//                     </a>
+//                     {index !== technologies.length - 1 ? "," : "."}
+//                   </React.Fragment>
+//                 ))}
+//               </span>
+//               I am a team player and always looking for new challenges and ways
+//               to improve myself.
+//             </p>
+//           </div>
+//         </div>
+//         {/* about me section  */}
+//         <AboutMe />
+//       </div>
+//       {/* <Footer /> */}
+//     </div>
+//   );
+// };
+
+// export default Home;
